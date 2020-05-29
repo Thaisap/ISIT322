c899d1d6988d313d1348320e7ce2804a05c057ed
KenEvans <ken.evans@bellevuecollege.edu>
2020-05-28 22:59:46 -0700

    Adding the new user profile activity
    TODO: Needs all the code behind

---
+++ Tudors/app/src/main/java/com/example/tudors/ScrollingActivityNewUserScreen.kt
@@ -0,0 +1,38 @@
+package com.example.tudors
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+class ScrollingActivityNewUserScreen : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling_new_user_screen)
+
+        val spinner: Spinner = findViewById(R.id.newUserSubjectSpinner)
+        spinner.onItemSelectedListener = this
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(this, R.array.subjects_array,
+            android.R.layout.simple_spinner_item).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        // Another interface callback
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        TODO("Not yet implemented")
+    }
+}
