86434c763f3dbc9012712566ef300331d0b64ba8
Dylan Harkness <dylanharkness@gmail.com>
2020-05-30 14:15:24 -0700

    GPS main screen

--- Tudors/app/src/main/java/com/example/tudors/MainScreenActivity.kt
+++ Tudors/app/src/main/java/com/example/tudors/MainScreenActivity.kt
@@ -1,11 +1,25 @@
 package com.example.tudors
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.os.Looper
+import android.provider.Settings
+import android.widget.*
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
 class MainScreenActivity : AppCompatActivity() {
+
+    val PERMISSION_ID = 42
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,8 +27,10 @@ class MainScreenActivity : AppCompatActivity() {
 
         val spinner: Spinner = findViewById(R.id.subject_spinner)
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter.createFromResource(this, R.array.subjects_array,
-            android.R.layout.simple_spinner_item).also { adapter ->
+        ArrayAdapter.createFromResource(
+            this, R.array.subjects_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             // Apply the adapter to the spinner
@@ -23,5 +39,115 @@ class MainScreenActivity : AppCompatActivity() {
 
         // Dylan (5/27/20): Not yet fully functional.
         // Add in https://developer.android.com/guide/topics/ui/controls/spinner#SelectListener
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val toggle: ToggleButton = findViewById(R.id.btnToggleGPS)
+        toggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // The toggle is enabled
+                getLastLocation()
+            } else {
+                // The toggle is disabled
+                findViewById<TextView>(R.id.textViewLocationResult).text = ""
+            }
+        }
+    }
+
+    private fun getAddress(lat: Double, lng: Double): String {
+        val geocoder = Geocoder(this)
+        val list = geocoder.getFromLocation(lat, lng, 1)
+        return list[0].getAddressLine(0)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        val address = getAddress(location.latitude, location.longitude)
+                        findViewById<TextView>(R.id.textViewLocationResult).text = address
+                        //findViewById<TextView>(R.id.textViewLocationResult).text =
+                        //    location.latitude.toString() + location.longitude.toString()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+
+            val address = getAddress(mLastLocation.latitude, mLastLocation.longitude)
+            findViewById<TextView>(R.id.textViewLocationResult).text = address
+            //findViewById<TextView>(R.id.textViewLocationResult).text =
+            //    mLastLocation.latitude.toString() + mLastLocation.longitude.toString()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLastLocation()
+            }
+        }
     }
 }
